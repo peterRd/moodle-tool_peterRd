ca483b26b5fa3e9d1a0a5d014dbaa60d6f1100ce
Peter <peter@moodle.com>
2018-09-21 14:31:56 +0800

    New dev test tool

---
+++ README.md
@@ -0,0 +1,1 @@
+New plugin creation test
---
+++ version.php
@@ -0,0 +1,5 @@
+<?php
+
+$plugin->component = 'block_foobar';
+$plugin->version = 2018092100; // Plugin released on 21th September 2019.
+$plugin->release = '1.0';
